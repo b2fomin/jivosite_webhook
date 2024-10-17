20fd374d39188866105f5d4b6f5f6210d820cf6a
Nikita <fomin.nv@phystech.edu>
2024-10-17 13:29:25 +0300

    creation migrations

---
+++ database/migrations/2024_10_17_102457_create_jivosite_webhook_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jivosite_webhook', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_name');
+            $table->unsignedBigInteger('widget_id');
+            $table->string('visitor_name');
+            $table->string('visitor_email');
+            $table->string('visitor_phone');
+            $table->string('visitor_description');
+            $table->string('visitor_number');
+            $table->unsignedBigInteger('chat_id');
+            $table->string('session_geoip_country');
+            $table->string('session_geoip_region');
+            $table->string('session_geoip_city');
+            $table->string('session_utm_json_google');
+            $table->string('session_utm_json_campaign');
+            $table->string('session_utm_json_content');
+            $table->string('session_utm_json_medium');
+            $table->string('session_utm_json_term');
+            $table->string('page_title');
+            $table->string('page_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('webhooks');
+    }
+};
